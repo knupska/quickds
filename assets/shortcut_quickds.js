6967a17293040f526de8e71ef2a7274c746fff58
Nathan Martin <nathan@knupska.com>
2010-04-07 00:07:43 +1000

    Fixed issue with shortcut in 2.0.6

--- assets/shortcut_quickds.js
+++ assets/shortcut_quickds.js
@@ -1,10 +1,13 @@
 jQuery(document).ready(function() {
 	
-	Symphony.Language.add({
-	    'Go to QuickDS' : false
-	});
-	
-	// Add the 'show/hide all' button
-	jQuery('form h2').append('<a class="button" title="Go to QuickDS" href="' + Symphony.WEBSITE + '/symphony/extension/quickds/edit/">' + Symphony.Language.get('Go to QuickDS') + '</a>');
-
+	// Add the QuickDS shortcut for 2.0.7+
+	if(Symphony.Language.add) {
+		Symphony.Language.add({'Go to QuickDS':false});
+		jQuery('form h2').append('<a class="button" title="Go to QuickDS" href="' + Symphony.WEBSITE + '/symphony/extension/quickds/edit/">' + Symphony.Language.get('Go to QuickDS') + '</a>');
+		
+	// Add the QuickDS shortcut for 2.0.6
+	} else {
+		$website = jQuery('script[src]')[0].src.match('(.*)/symphony')[1];
+		jQuery('form h2').append('<a class="button" title="Go to QuickDS" href="' + $website + '/symphony/extension/quickds/edit/">' + 'Go to QuickDS' + '</a>');
+	}
 });
